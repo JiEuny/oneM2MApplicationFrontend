61910679f585f501bbd95bcf4825dabd7a686eda
jieun <love9ly@gmail.com>
2020-10-05 11:58:44 +0900

    commit

---
+++ src/components/Monitoring.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="grid-content bg-purple-light">
+      <pre>{{ JSON.stringify(response, null, "\t") }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      response: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:3000/notify").then((res) => {
+      console.log(res.data);
+      // alert(res.data)
+      this.response = JSON.parse(res.data);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.bg-purple-light {
+  background: #e5e9f2;
+  text-align: left;
+  margin: 10% 10% 10% 10%;
+  min-height: 1000px;
+  font-size: 1.5em;
+}
+</style>
